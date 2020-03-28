16d4ce9a68d52cd2981f76a0de50ca791d725888
Alessandro Astone <ales.astone@gmail.com>
2020-03-28 12:09:07 +0100

    RMX1851: Disable SF backpressure
    Change-Id: I4d5e9ec52901a8b7da9c0c439417a4ac90c512b3
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -47,7 +47,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.enable_hwc_vds=1 \
-    debug.sf.enable_gl_backpressure=1 \
+    debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
