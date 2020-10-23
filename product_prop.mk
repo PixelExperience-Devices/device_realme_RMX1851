4d266e54d1b5f9500ce24977fb0b715ceb88225f
SamarV-121 <samarvispute121@gmail.com>
2020-10-23 20:46:10 +0530

    RMX1851: Optimize ART
     * Ref: https://source.android.com/devices/tech/dalvik/configure#dex2oat_options
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -36,6 +36,17 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.lcd_density=440
 
+# Dex2oat
+PRODUCT_PRODUCT_PROPERTIES += \
+    dalvik.vm.image-dex2oat-filter=quicken \
+    dalvik.vm.image-dex2oat-threads=8 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.dex2oat-filter=quicken \
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7
+
 # FM
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.fm.use_audio_session=true
