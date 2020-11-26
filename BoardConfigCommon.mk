4345836f444d352939c561564d211c0534c59a82
SamarV-121 <samarvispute121@gmail.com>
2020-11-26 18:40:01 +0530

    sdm710-common: Remove unused rootfs symlinks
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,13 +80,6 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
 
-# Root
-BOARD_ROOT_EXTRA_SYMLINKS := \
-    /mnt/vendor/persist:/persist \
-    /vendor/bt_firmware:/bt_firmware \
-    /vendor/dsp:/dsp \
-    /vendor/firmware_mnt:/firmware
-
 # Recovery
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/recovery.fstab
 
