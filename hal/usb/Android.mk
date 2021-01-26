e7abf16da7eef1b9d2c18f2d23371ee21757c15c
SamarV-121 <samarvispute121@gmail.com>
2021-01-26 20:33:48 +0530

    Revert "RMX1851: hal/usb: Switch to system varient"
    This reverts commit be6365ccf39b2f003e9ab21f895e4ba031b443d0.
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: I1b8bfb98de0f57d5e5c1b5bec27d61972a6216f8
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- hal/usb/Android.mk
+++ hal/usb/Android.mk
@@ -1,32 +1,23 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := android.hardware.usb@1.0-service.realme_sdm710.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/init
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-
-include $(BUILD_PREBUILT)
-
 LOCAL_PATH := hardware/custom/interfaces/usb/1.0-basic
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.usb@1.0-service.realme_sdm710
 LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.usb@1.0-service
 
 LOCAL_SRC_FILES := service.cpp Usb.cpp
-
-LOCAL_REQUIRED_MODULES := $(LOCAL_MODULE).rc
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libhidlbase \
     libhardware \
-    libbase \
-    libutils \
     android.hardware.usb@1.0
+
+LOCAL_STATIC_LIBRARIES := \
+    libbase \
+    libutils
 
 include $(BUILD_EXECUTABLE)
